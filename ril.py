9ba871c1733e8bdd109a891be1de569a8a620a17
bravmech <bravmech@gmail.com>
2014-03-26 17:08:59 +0600

    ril 1.1 alpha, kinda works

--- ril.py
+++ ril.py
@@ -3,7 +3,7 @@
     RIL2.0
     ~~~~~~
 
-    RIL taken to the next level.
+    RIL to be taken to the next level.
 
     :copyright: (c) 2014 by bravmech
 """
@@ -11,81 +11,97 @@
 from flask import Flask, g
 from flask import redirect, url_for, render_template
 from flask import session, request, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 import os
 import ipdb
 import datetime as dt
-from sqlite3 import dbapi2 as sqlite3
+from functools import wraps
+
 from utils import *
 
-app.secret_key = 'omsVT1zPKmBhPMEVNlVQvgryp'
 
+# basically it's config
+app.secret_key = 'omsVT1zPKmBhPMEVNlVQvgryp'
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'ril.db'),
+    SQLALCHEMY_DATABASE_URI='sqlite:////%s/ril.db' % app.root_path,
     DEBUG=True,
     SECRET_KEY='development key',
 ))
-
 app.jinja_env.globals.update(isurl=isurl)
+db = SQLAlchemy(app)
 
 
-def connect_db():
-    """Connects to the specific database."""
-    conn = sqlite3.connect(app.config['DATABASE'],
-                            detect_types=sqlite3.PARSE_DECLTYPES)
-    conn.row_factory = sqlite3.Row
-    return conn
+# =db stuff
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    pw_hash = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, pw_hash, email):
+        self.name = name
+        self.pw_hash = pw_hash
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.name
 
 
-def init_db():
-    """Creates the database tables."""
-    if os.path.exists(app.config['DATABASE']):
-        return
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    state = db.Column(db.String(20)) # enum?
+    added = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    def __init__(self, content, user_id):
+        self.content = content
+        self.state = 'unread'
+        self.added = dt.datetime.now()
+        self.user_id = user_id
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    def __repr__(self):
+        return '<Item %r>' % self.content
 
 
 def create_user(username, password, email):
-    db = get_db()
-    db.execute('insert into users(name, pw_hash, email) values (?, ?, ?)',
-                [username, make_pw_hash(username, password), email])
-    db.commit()
+    u = User(username, make_pw_hash(username, password), email)
+    db.session.add(u)
+    db.session.commit()
 
 
 def get_user(username):
-    db = get_db()
-    cur = db.execute('select id, name, pw_hash, email from users where name = ?',
-                        [username])
-    return cur.fetchone()
+    return User.query.filter_by(name=username).first()
 
 
 def get_item(item_id):
-    db = get_db()
-    cur = db.execute('''
-        select id, user_id, content, state, added from items
-        where id = ?
-    ''', [item_id])
-    return cur.fetchone()
+    return Item.query.filter_by(id=item_id).first()
+
+
+# =decorators custom
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def verify_item(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        item_id = kwargs['item_id']
+        item = get_item(item_id)
+        if g.user and item.user_id != g.user.id:
+            flash('Please edit your own items.')
+            return redirect('/unread')
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.before_request
@@ -177,17 +193,10 @@ def new_item():
     if request.method == 'GET':
         return render_template('new.html', username=session['username'])
 
-    user_id = g.user[0]
-    content = request.form['content']
-    state = 'unread'
-    added = dt.datetime.now()
+    item = Item(content=request.form['content'], user_id=g.user.id)
+    db.session.add(item)
+    db.session.commit()
 
-    db = get_db()
-    db.execute('''
-        insert into items(user_id, content, state, added)
-        values (?, ?, ?, ?)
-    ''', [user_id, content, state, added])
-    db.commit()
     flash('new item aded')
     return redirect('/unread')
 
@@ -205,55 +214,43 @@ def show_list(state):
     if 'username' not in session:
         return redirect(url_for('login'))
     # ipdb.set_trace()
-    db = get_db()
-    cur = db.execute('''
-        select id, user_id, content, state, added from items
-        where state = :state and items.user_id = :user_id
-    ''', dict(state=state, user_id=g.user[0]))
-    items = cur.fetchall()
+    items = Item.query.filter_by(state=state, user_id=g.user.id)
     return render_template('list.html', items=items, username=session['username'],
                             state=state)
 
 
-def verify_item(item_id):
-    item = get_item(item_id)
-    user_id = item[1]
-    if user_id != g.user[0]:
-        flash('Please edit your own items.')
-        return redirect('/unread')
-    return item
-
-
+@verify_item
 @app.route('/check/<int:item_id>')
 def check(item_id):
-    item = verify_item(item_id)
-    db = get_db()
-    db.execute("update items set state = 'read' where id = ?", [item[0]])
-    db.commit()
+    item = get_item(item_id)
+    item.state = 'read'
+    item.added = dt.datetime.now()
+    db.session.commit()
     flash('item marked as read')
     return redirect('/unread')
 
 
+@verify_item
 @app.route('/re-add/<int:item_id>')
 def re_add(item_id):
-    item = verify_item(item_id)
-    db = get_db()
-    db.execute("update items set state = 'unread' where id = ?", [item[0]])
-    db.commit()
+    item = get_item(item_id)
+    item.state = 'unread'
+    item.added = dt.datetime.now()
+    db.session.commit()
     flash('item marked as unread')
     return redirect('/read')
 
 
+@verify_item
 @app.route('/delete/<int:item_id>')
 def delete(item_id):
-    item = verify_item(item_id)
-    db = get_db()
-    db.execute("delete from items where id = ?", [item[0]])
-    db.commit()
+    item = get_item(item_id)
+    db.session.delete(item)
+    db.session.commit()
     flash('item deleted')
     return redirect('/read')
 
 
 if __name__ == '__main__':
-    init_db()
+    db.create_all()
     app.run(debug=True)
